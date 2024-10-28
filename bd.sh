9aec05cdef1d7c0a12fafc3e7c188c888e3b3311
joanneeqin <chongeqin1029@gmail.com>
2024-10-28 22:39:00 +0800

    add sjtu_drone submodule

--- bd.sh
+++ bd.sh
@@ -1,3 +1,8 @@
+git submodule update --init --recursive
+cd sjtu_drone
+colcon build --symlink-install
+cd ..
+
 . params.sh
 
 colcon build --symlink-install #--cmake-args -DCMAKE_BUILD_TYPE=Release #--packages-select my_package
--- clean_bd.sh
+++ clean_bd.sh
@@ -1,3 +1,9 @@
+git submodule update --init --recursive
+cd sjtu_drone
+rm -rf build install log
+colcon build --symlink-install
+cd ..
+
 . params.sh
 
 rm -rf build install log
